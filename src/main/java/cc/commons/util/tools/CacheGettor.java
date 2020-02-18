1ef02c434cb326b9b3c0334326622606d37a298b
聪聪 <178666380@qq.com>
2020-02-18 15:07:40 +0800

    s增加Warp的Get方式

--- src/main/java/cc/commons/util/tools/CacheGettor.java
+++ src/main/java/cc/commons/util/tools/CacheGettor.java
@@ -2,34 +2,45 @@ package cc.commons.util.tools;
 
 import cc.commons.util.interfaces.IGettor;
 
-public class CacheGettor<T>{
+public class CacheGettor<T> {
 
-    public static <E> CacheGettor<E> create(IGettor<E> pGettor){
+    public static <E> CacheGettor<E> create(IGettor<E> pGettor) {
         return new CacheGettor(pGettor);
+    }
+
+    public static <E> CacheGettor<E> warp(E pValue) {
+        CacheGettor<E> tGet = new CacheGettor();
+        tGet.mCacheValue = pValue;
+        tGet.mCached = true;
+        return tGet;
     }
 
     /** Gettor,获取值 */
     protected final IGettor<T> mGettor;
     /** 缓存的值,无值时为null */
-    protected T mCacheValue=null;
+    protected T mCacheValue = null;
     /** 标记是否已经缓存值 */
-    private boolean mCached=false;
+    private boolean mCached = false;
 
-    public CacheGettor(IGettor<T> pGettor){
-        this.mGettor=pGettor;
+    private CacheGettor() {
+        this.mGettor = null;
+    }
+
+    public CacheGettor(IGettor<T> pGettor) {
+        this.mGettor = pGettor;
     }
 
     /**
      * 使缓存的值失效
      */
-    public void makerInvalid(){
-        synchronized(this){
-            this.mCached=false;
-            this.mCacheValue=null;
+    public void makerInvalid() {
+        synchronized(this) {
+            this.mCached = false;
+            this.mCacheValue = null;
         }
     }
-    
-    public boolean cached(){
+
+    public boolean cached() {
         return this.mCached;
     }
 
@@ -38,11 +49,11 @@ public class CacheGettor<T>{
      * 
      * @return 数据
      */
-    public T get(){
-        synchronized(this){
-            if(!this.mCached){
-                this.mCacheValue=this.mGettor.get();
-                this.mCached=true;
+    public T get() {
+        synchronized(this) {
+            if (!this.mCached) {
+                this.mCacheValue = this.mGettor.get();
+                this.mCached = true;
             }
 
             return this.mCacheValue;
